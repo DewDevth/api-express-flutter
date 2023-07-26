b938336c37dd32d0fa3e9b2576200aa08fdf5f88
DewDevth <140059595+DewDevth@users.noreply.github.com>
2023-07-26 08:36:58 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRouter = require("./routes/auth");
+const cors = require("cors");
+const PORT = process.env.PORT || 3000;
+const app = express();
+const allowedOrigins = [
+  "http://localhost:40494", // Replace with the origin of your frontend application
+  // Add more origins as needed
+];
+
+// Use the cors middleware to enable Cross-Origin Resource Sharing
+app.use(cors(allowedOrigins));
+
+app.use(express.json());
+app.use(authRouter);
+
+const DB =
+  "mongodb+srv://admin:1234@cluster0.mah5v2u.mongodb.net/?retryWrites=true&w=majority";
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection database Successfully");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`connected at port ${PORT}`);
+});
---
+++ routes/auth.js
@@ -0,0 +1,69 @@
+const bcryptjs = require("bcryptjs");
+const express = require("express");
+const User = require("../models/user");
+const authRouter = express.Router();
+const jwt = require("jsonwebtoken");
+
+authRouter.get("/", async (req, res) => {
+  res.send("Hello world");
+});
+//register
+authRouter.post("/api/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res
+        .status(400)
+        .json({ msg: "User with same email already exists!" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 8);
+
+    let user = new User({
+      email,
+      password: hashedPassword,
+      name,
+    });
+    user = await user.save();
+    res.json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+//login
+authRouter.post("/api/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ msg: "User with this email does not exist!" });
+    }
+
+    const isMacth = await bcryptjs.compare(password, user.password);
+    if (!isMacth) {
+      return res.status(400).json({ msg: "Incorrect password." });
+    }
+    const token = jwt.sign({ id: user._id }, "passwordKey");
+    res.json({ token, ...user._doc });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+authRouter.get("/api/users", async (req, res) => {
+  try {
+    const user = await User.find();
+    console.log("route users");
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+module.exports = authRouter;
